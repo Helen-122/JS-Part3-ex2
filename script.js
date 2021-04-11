17dc67164fac0dc8d5e2ce0d3748c4da5c20f8fa
Helen-122 <helene.mazourine@gmail.com>
2021-04-11 20:15:34 +0200

    js part 3 ex 2

---
+++ script.js
@@ -0,0 +1,10 @@
+var text = document.getElementById("text");
+
+//Pour masquer le texte :
+function displayNone(){
+  text.style.visibility = "hidden";
+}
+//Pour afficher le texte :
+function displayBlock(){
+  text.style.visibility = "visible";
+}
